46979328d172e2ffe2f071464e7c6252ba13a70d
Vilson1984 <vcmorion@hotmail.com>
2022-04-04 16:17:06 -0300

    add CSS and modifying form

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,18 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_04_02_004949) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "nome_evento"
+    t.date "data_conclusao"
+    t.string "categoria"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "itens", force: :cascade do |t|
     t.string "nome_evento"
     t.string "data_conclusão"
