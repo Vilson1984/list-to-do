998d21fcf69472b9b538111e4d30539bc3d9a150
Vilson1984 <vcmorion@hotmail.com>
2022-04-05 17:25:35 -0300

    modifying route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :itens
+  # root 'yplan#index'
+   resources :index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
